2d5e3a1d50e93ee5fcb7e80e333ffecc8fc468ce
MarcHarnist <31386370+MarcHarnist@users.noreply.github.com>
2020-07-09 15:53:29 +0200

    Delete controllers and models
    To replace them immediately

--- controllers/__client-deconnection.php
+++
@@ -1,8 +0,0 @@
-
-<?php
-
-if (isset($_SESSION['client'])){ // If session client exists, restore object
-	unset($client);
-	unset($_SESSION['client']);
-	// session_destroy(); // The session is closed
-}
--- models/class-uploader.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// This file is required in www.index.php
-
-// Function upload OOP classes if an object is created
-function class_upload($classname) { require 'classes/' . $classname.'.php';}
-spl_autoload_register('class_upload');
