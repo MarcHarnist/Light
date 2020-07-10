975a97591a63cc43b8f014a25d5bf9e727cc9103
MarcHarnist <31386370+MarcHarnist@users.noreply.github.com>
2020-07-10 12:52:51 +0200

    Update bac

---
+++ view/bac.php
@@ -0,0 +1,29 @@
+<!--
+                                 Page   Bac
+									
+	File : root/view/bac.php (bac = bac à sable = sandbox)
+	Author : Marc L. Harnist
+	Date : 2020-07-01
+	
+	Thema
+	Try to display config file constants to edit them
+		    // Essaie d'afficher les variables de config
+
+    Idea
+    Using model
+    The model (config-file-reading) is automous. He has html, css and php code inside. 
+    So it can be imported anywhere. 
+
+-->
+<article>
+  <header class="row">
+    <h2 class="col-lg-12 ml-0 mt-4 ">Bac</h2>
+  </header>
+  <section class="col-lg-12 ">
+    <h3 class="h5"><i>Baccasable</i></h3>
+	<p>Travaux en cours: sauvegarde des commentaires du fichier root/config/config.php<br>
+	Fichier modifié: <strong><u><big>models</big></u></strong>/config-file-read.php line ~ 73<br>
+	<a href="index.php?page=config-file-read"><u>Page html qui affichera le résultat.</u></a>
+	<p>Création des routeurs "client", "member", "public".<a href="index.php?space=client&page=tableau-de-bord">Tester</a></p>
+  </section>
+</article>
