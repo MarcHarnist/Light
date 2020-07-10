c7ae4e6c977fe60465c89d276c2f607ce0a92b04
MarcHarnist <31386370+MarcHarnist@users.noreply.github.com>
2020-07-10 06:44:17 +0200

    Create controller bac

---
+++ controllers/bac.php
@@ -0,0 +1,13 @@
+﻿<?php
+/**             Contrôleur client-update
+*                   Marc L. Harnist
+*                       28/05/2020
+*
+*   Autorisation limitée au webmaster
+*/  
+	$rightLevelThisPage = 1;// 1 = only administrator
+	
+	/* @var $message Type array used to display a message in case of false import or other operation */  
+	$message["adminOnly"] = "Il faut être connecté en tant qu'administrateur du site web";
+
+	isset($member)?$website->membersPermissions($rightLevelThisPage, $member):exit($message["adminOnly"]); 
