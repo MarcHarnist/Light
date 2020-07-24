017b88b145b9f3648b25ddd8edb7edcf00be810e
MarcHarnist <31386370+MarcHarnist@users.noreply.github.com>
2020-07-24 17:16:20 +0200

    Update page-index.php
    Display all the article and delete extract display and blue button

--- view/pages-index.php
+++ view/pages-index.php
@@ -11,34 +11,27 @@ foreach($pages as $page_en_cours_de_lecture)
 {
 	?>
 	<article>
-	<header>
-		<h3 id="<?=$page_en_cours_de_lecture['id'];?>">
-			<?php echo '<a href="' . $website->page_url . 'page-from-pages-index&amp;id='. $page_en_cours_de_lecture['id'] . '&amp;titre=' . $page_en_cours_de_lecture['url']. '" target="_blank">'.$page_en_cours_de_lecture['title'].'</a>';
-			?>
-			<em><small><small>
-			<?php
-			if (isset($member) && $member->level <= 2){
-			
-				// The user has enough permissions, display the edition-link
-				// Le menu d'édition, de création de pages ou de suppression
-				include("view/".'__menu-edition.php');
-			}
-			?>
-			<br />
-			Le <?=$page_en_cours_de_lecture['date'];?></em> <?php if($page_en_cours_de_lecture['author'] != "") echo "Auteur: " . $page_en_cours_de_lecture['author'];?></small></small>
-		</h3>
-	</header>
-	
-	<section><?=$page_en_cours_de_lecture['text'];?></section>
-
-		<?php if (isset($editor) && $editor == True): //User has enough permissions ?>
-			<p class="icon">
-			<?php include_once("view/".'__menu-edition.php');?>
-		<?php endif; ?>
-	</p>
-	
-	
-	
+		<header>
+			<h3 id="<?=$page_en_cours_de_lecture['id'];?>">
+				<?php echo '<a href="' . $website->page_url . 'page-from-pages-index&amp;id='. $page_en_cours_de_lecture['id'] . '&amp;titre=' . $page_en_cours_de_lecture['url']. '" target="_blank">'.$page_en_cours_de_lecture['title'].'</a>';
+				?>
+				<em><small><small>
+				<?php 
+				if ($editor_display) include("view/".'__menu-edition.php'); ?>
+				<br />
+				<?php if(BLOG_DATE_DISPLAY === "yes"): ?>
+				Le <?=$page_en_cours_de_lecture['date'];?></em> 
+				<?php endif;?>
+				
+				<?php if(BLOG_AUTHOR_DISPLAY === "yes" && $page_en_cours_de_lecture['author'] != "") echo "Auteur: " . $page_en_cours_de_lecture['author'];?></small></small>
+			</h3>
+		</header>
+		<section>
+			<?=$page_en_cours_de_lecture['text'];?>
+		</section>
+					<?php if($editor_display):?>
+		<p class="icon"><?php include_once("view/".'__menu-edition.php');?></p>
+					<?php endif; ?>
 	</article>
 	<?php
 }
