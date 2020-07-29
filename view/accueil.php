ac6897382d4ea190acb3df39c3ea1af64aa9e259
MarcHarnist <31386370+MarcHarnist@users.noreply.github.com>
2020-07-29 21:05:49 +0200

    Update homepage (accueil.php) & inc/menu-edition

--- view/accueil.php
+++ view/accueil.php
@@ -5,23 +5,17 @@
 	Author : Marc Harnist
 	Date : 2020-6-30
 -->
-<header>
-    <h2>Accueil</h2>
-</header>
 	<?php
 	  //Si la connexion se passe mal, un message s'affiche ici
 	  if((isset($message) && Null !== $message && $message->getRed()) != ""){
 		echo $website::message("Petite erreur", $message->getRed(), "pink");
 	  }
 	?>
-<article>
 
 	<?=$page_en_cours_de_lecture['text'];?>
 
 	<?php if($editor_display):?>
 
-	<p class="icon"><?php include_once("view/".'__menu-edition.php');?></p>
+	<span class="icon"><?php include_once('inc/menu-edtion.php');?></span>
 
 	<?php endif; ?>
-
-</article>
