4382a558782e59be286361506e854cfc91c2e14d
MarcHarnist <31386370+MarcHarnist@users.noreply.github.com>
2020-07-29 16:48:19 +0200

    Update header
    Font awesome links restored

--- inc/header.php
+++ inc/header.php
@@ -8,9 +8,9 @@
 		<title><?php echo WEBSITE_NAME . ' '; if(isset($page)) echo $page->getTitle();?></title>
 
 		<!-- Awesome font -->
-		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
 		<script defer src="https://use.fontawesome.com/releases/v5.0.13/js/all.js"></script>
-		
+		<script defer src="https://use.fontawesome.com/releases/v5.0.13/js/v4-shims.js"></script>		
 		<script src="ckeditor/ckeditor.js"></script>
 		
 		<!-- Bootstrap (look at the bootom too) -->
