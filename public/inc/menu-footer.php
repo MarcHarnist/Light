c0e02b93886273df8a001ed58e5bd38d43d0c38c
MarcHarnist <31386370+MarcHarnist@users.noreply.github.com>
2020-08-12 07:30:29 +0200

    Update menu-footer

--- public/inc/menu-footer.php
+++ public/inc/menu-footer.php
@@ -1,26 +1,32 @@
 	<menu class="footer">
-		<ul class="nav col-lg-3 col-md-6"><!-- usefull to W3C-validator -->
-			<li class="nav-item">
-				<a 	class="mr-2" title = "Mentions légales" href="Mentions-legales">
-					<i class="fas fa-gavel fa"></i></a>
-			</li>
-			<li class="nav-item">
-				<a  class="mx-2 text-light" title = "Admin" href="connexion">
-					<i class="fa fa-user" aria-hidden="true"></i>
-				</a>
-			</li>
-			<?php
-			if(isset($_SESSION['member'])): ?>
-				<li class="nav-item"><!-- MEMBER SPACE DECONNECTION BUTTON -->
-					<a  class="mx-2" title = "Se déconnecter" 
-					href="index.php?page=__member-deconnection">
+	<ul class="nav col-md-12 col-lg-8 h6 justify-content-end"><!-- usefull to W3C-validator -->
+				<li class="nav-item">
+					<a 	class="mr-2" 
+						title = "Mensions légales du site web"
+						href="Mentions-legales">
+						<i class="fas fa-gavel fa"></i></a>
+				</li>
+				<li class="nav-item">
+					<a  class="mx-2 text-light"
+						title = "Se connecter à l'administration du site"
+						href="connexion">
+						<i class="fa fa-user" aria-hidden="true"></i>
+					</a>
+				</li>
+					<!-- ESPACE MEMBRE - BOUTONS DE CONNEXION -->
+					<?php
+					if(isset($_SESSION['member'])){
+						?>
+				<li class="nav-item">
+					<a  class="mr-2"
+						title = "Se déconnecter"
+						href="<?=$website->page_url;?>__member-deconnection">
 						<i class="fa fa-power-off text-danger" aria-hidden="true"></i>
 					</a>
 				</li>
-				<?php
-			endif; ?>
-			<li class="nav-item"><!-- MEMBER SPACE DECONNECTION BUTTON -->
-			<address class="text-center">Projet de <a href="http://zaabel.fr" target="_blank">Zaabel</a></address>
-			</li>
-		</ul>
+						<?php
+						}
+					?>
+			</p><address>Propulsé par <a target="_blank" href="http://marcharnist.fr/light" title="CMS Light">"Light"</a>, CMS exigeant peu de ressources créé par <a target="_blank" href="http://marcharnist.fr" title="L'auteur"><?=WEBMASTER;?></a>.</address>
+	
 	<menu>
